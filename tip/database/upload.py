330cdf30d7af2f5d3a13bc932de46023f6075a65
Jason Youn <jyoun@ucdavis.edu>
2020-06-23 00:17:59 -0700

    code review added

--- tip/database/upload.py
+++ tip/database/upload.py
@@ -1,25 +1,37 @@
+# file header should go here
+# what does this file do?
+# who are the authors?
+# any to-dos?
+# please find a good file docstring format and use it for all files
+
 import csv
 import numpy as np
 import pandas as pd
 
-cols_compound   = open('compound.txt', 'r').read().split('\n')
-cols_assay      = open('assay.txt', 'r').read().split('\n')
+cols_compound = open('compound.txt', 'r').read().split('\n')
+cols_assay = open('assay.txt', 'r').read().split('\n')
+
 
 def convert(fobj_or_path):
-    """convert the filled CSV template file into JSON format
-    input:
-        fobj_or_path    : (file object or str) file
-    output:
-        data            : (pd.DataFrame)"""
+    """Convert the filled CSV template file into JSON format.
+
+    Args:
+        fobj_or_path (file object or str): SOME MEANINGFUL DESCRIPTION.
+
+    Returns:
+        data (pd.DataFrame) : SOME MEANINGFUL DESCRIPTION.
+    """
 
     def convert_row(se, n_assay):
-        """convert a row of dataframe into organized JSON format
-        input:
-            se          : (pd.Series) a row of dataframe
-            n_assay     : (int) amount of assays associated with the compound
-        output:
-            row         : (dict) JSON-fied row"""
+        """Convert a row of dataframe into organized JSON format.
 
+        Args:
+            se (pd.Series): A row of dataframe.
+            n_assay (int): Amount of assays associated with the compound.
+
+        Returns:
+            row (dict): JSON-fied row.
+        """
         row = {}
         assays = []
 
@@ -38,20 +50,23 @@ def convert(fobj_or_path):
 
         return row
 
-    data = pd.read_csv(fobj_or_path, dtype = str)
+    data = pd.read_csv(fobj_or_path, dtype=str)
     n_assay = int(data.columns.tolist()[-1][-1])
-    data = data.apply(lambda se: convert_row(se, n_assay), axis = 1)
+    data = data.apply(lambda se: convert_row(se, n_assay), axis=1)
 
     return data
 
-def generate_template(path, n_assay):
-    """generate a template CSV file to the given path
-    input:
-        path            : (str) location
-        n_assay         : (int) amount of assays associated with the compound
-    output:
-        (None)"""
 
+def generate_template(path, n_assay):
+    """Generate a template CSV file to the given path.
+
+    Args:
+        path (str): Location.
+        n_assay (int): Amount of assays associated with the compound.
+
+    Returns:
+        None
+    """
     cols = cols_compound
     for i in range(n_assay):
         cols_assay_batch = [s + str(i + 1) for s in cols_assay]
@@ -60,7 +75,7 @@ def generate_template(path, n_assay):
     writer = csv.writer(open(path, 'w'))
     writer.writerow(cols)
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     generate_template('template.csv', 2)
     convert('template.csv')
