230c896e1250dac60b617647b8ed74ef020e458c
fangzhouli <fzli0805@gmail.com>
2020-09-17 17:04:40 -0700

    renamed and removed functions in __all__

--- tip/utils/__init__.py
+++ tip/utils/__init__.py
@@ -1,5 +1,4 @@
-from .set_logging import set_logging
-from .split_query import split_query
+from .split_values import split_values
 from .convert_csv_to_json import convert_csv_to_json
 
-__all__ = ['set_logging', 'split_query', 'convert_csv_to_json']
+__all__ = ['split_values', 'convert_csv_to_json']
